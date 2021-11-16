802ae04c83137368b489a793ca90fa96c507abe0
hydroSs1 <50531243+hydroSs1@users.noreply.github.com>
2021-11-16 23:22:25 +0100

    First Commit

---
+++ gestioneschermo.d
@@ -0,0 +1,11 @@
+.\gestioneschermo.o: gestioneSchermo\gestioneSchermo.c
+.\gestioneschermo.o: gestioneSchermo\../GLCD/GLCD.h
+.\gestioneschermo.o: C:\Users\miche\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.6.0\Device\Include\LPC17xx.h
+.\gestioneschermo.o: C:\Users\miche\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm3.h
+.\gestioneschermo.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\gestioneschermo.o: C:\Users\miche\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h
+.\gestioneschermo.o: C:\Users\miche\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h
+.\gestioneschermo.o: C:\Users\miche\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armcc.h
+.\gestioneschermo.o: C:\Users\miche\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\mpu_armv7.h
+.\gestioneschermo.o: C:\Users\miche\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.6.0\Device\Include\system_LPC17xx.h
+.\gestioneschermo.o: gestioneSchermo\../TouchPanel/TouchPanel.h
