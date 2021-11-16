89a8d29d9e30c802bba20ac9d6cdedb11816b476
hydroSs1 <50531243+hydroSs1@users.noreply.github.com>
2021-11-16 23:24:47 +0100

    First Commit

---
+++ labirinto.d
@@ -0,0 +1,5 @@
+.\labirinto.o: labirinto\labirinto.c
+.\labirinto.o: labirinto\labirinto.h
+.\labirinto.o: labirinto\../timer/timer.h
+.\labirinto.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\labirinto.o: labirinto\../gestioneSchermo/gestioneSchermo.h
